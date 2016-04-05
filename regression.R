892f347076d579c3cefe21d87f273e815e5c3db2
Tdarra <taj_darra@windowslive.com>
2016-04-05 00:01:51 -0700

    Fixed intercept error
    use this to load the data and begin regression

--- regression.R
+++ regression.R
@@ -1,7 +1,35 @@
-#Reading the data in from local directory, setting row.names = 1 will make the Date our row index
-first_world_absolute_exp <- read.csv('FirstWorldAbsoluteExplantoryData.csv', row.names = 1)
-first_world_resp <- read.csv('FirstWorldEmissionData.csv', row.names = 1)
-first_world_relative_exp <- read.csv('FirstWorldRelativeExplantoryData.csv', row.names = 1)
-third_world_relative_exp <- read.csv('ThirdWorldRelativeExplanatoryData.csv', row.names = 1)
-third_world_absolute_exp <- read.csv('ThirdWorldAbsoluteExplanatoryData.csv', row.names = 1)
-third_world_response <- read.csv('ThirsdWorldEmissionData.csv', row.names = 1)
+#Analysis
+
+####Load the Data
+First_World_Resp <- read.csv('FirstWorldEmissionData.csv', header = TRUE, row.names = 1)
+First_World_Abs_Explanatory <- read.csv('FirstWorldAbsoluteExplantoryData.csv', header = TRUE, row.names = 1)
+First_World_Rel_Explanatory <- read.csv('FirstWorldRelativeExplantoryData.csv', header = TRUE, row.names = 1)
+Third_World_Resp <- read.csv('ThirsdWorldEmissionData.csv', header = TRUE, row.names = 1)
+Third_World_Abs_Explanatory <- read.csv('ThirdWorldAbsoluteExplanatoryData.csv', header = TRUE, row.names = 1)
+Third_World_Rel_Explanatory <- read.csv('ThirdWorldRelativeExplanatoryData.csv', header = TRUE, row.names = 1)
+
+
+
+#Take note of respnose set size, no need for explanatory data that does is not within response bounds
+y <- length(First_World_Resp$V2)
+j <- length(Third_World_Resp$V2)
+
+# trim sample data based on response set
+fw_rel <- First_World_Rel_Explanatory[1:y,]
+fw_abs <- First_World_Abs_Explanatory[1:y,]
+tw_rel <- Third_World_Rel_Explanatory[1:j,]
+tw_abs <- Third_World_Abs_Explanatory[1:j,]
+
+# augment the df's by the response sets
+fw_rel$emission <- First_World_Resp$V2
+fw_abs$emission <- First_World_Resp$V2
+tw_rel$emission <- Third_World_Resp$V2
+tw_abs$emission <- Third_World_Resp$V2
+
+attach(fw_abs) # remember to detach later, just makes using columns easier
+
+fw_abs_fit_allvars <- lm(formula = emission ~ ., data = fw_abs) # all variable fit
+summary(fw_abs_fit_allvars) # beauty
+
+
+
