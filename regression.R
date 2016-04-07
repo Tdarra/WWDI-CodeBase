991c23669901604992071e8a668cc111d32f4e59
joshua <joshua.dejong@gmail.com>
2016-04-07 14:31:46 -0700

    merged fw and tw data, added correlation matrices for both, and started fitting linear models

--- regression.R
+++ regression.R
@@ -12,7 +12,7 @@ Third_World_Rel_Explanatory <- read.csv('ThirdWorldRelativeExplanatoryData.csv',
 
 
 
-#Take note of respnose set size, no need for explanatory data that does is not within response bounds
+#Take note of response set size, no need for explanatory data that does is not within response bounds
 y <- length(First_World_Resp$V2)
 j <- length(Third_World_Resp$V2)
 
@@ -33,64 +33,85 @@ attach(fw_abs) # remember to detach later, just makes using columns easier
 fw_abs_fit_allvars <- lm(formula = emission ~ ., data = fw_abs) # all variable fit
 summary(fw_abs_fit_allvars) # beauty
 detach(fw_abs)
+
+# merge rel and abs into one dataset
+fw <- merge(fw_abs, fw_rel, by = c("row.names", 'emission'))
+tw <- merge(tw_abs, tw_rel, by = c("row.names", 'emission'))
+
 #delete all columns that have CO2E has they are defined as fractions of the response data
-fw_abs$CO2E_GF_KT...Value <- NULL
-fw_abs$CO2E_KD_GD...Value <- NULL
-fw_abs$CO2E_LF_KT...Value <- NULL
-fw_abs$CO2E_SF_KT...Value <- NULL
-fw_abs$CO2_BLDG_MT...Value <- NULL
-fw_abs$CO2_ETOT_MT...Value <- NULL
-fw_abs$ATM_CO2E_KT...Value <- NULL
-fw_abs$ATM_CO2E_PC...Value <- NULL
-fw_abs$CO2_MANF_MT...Value <- NULL
-fw_abs$CO2_OTHX_MT...Value <- NULL
-fw_abs$CO2_TRAN_MT...Value <- NULL
+fw$CO2E_GF_KT...Value <- NULL
+fw$CO2E_KD_GD...Value <- NULL
+fw$CO2E_LF_KT...Value <- NULL
+fw$CO2E_SF_KT...Value <- NULL
+fw$CO2_BLDG_MT...Value <- NULL
+fw$CO2_ETOT_MT...Value <- NULL
+fw$ATM_CO2E_KT...Value <- NULL
+fw$ATM_CO2E_PC...Value <- NULL
+fw$CO2_MANF_MT...Value <- NULL
+fw$CO2_OTHX_MT...Value <- NULL
+fw$CO2_TRAN_MT...Value <- NULL
 
-fw_rel$CO2E_EG <- NULL
-fw_rel$CO2E_GF <- NULL
-fw_rel$CO2E_LF <- NULL
-fw_rel$CO2E_SF<- NULL
-fw_rel$CO2_BLDG <- NULL
-fw_rel$CO2_ETOT <- NULL
-fw_rel$CO2_MANF <- NULL
-fw_rel$CO2_TRAN <- NULL
-fw_rel$CO2_OTHX <- NULL
+fw$CO2E_EG <- NULL
+fw$CO2E_GF <- NULL
+fw$CO2E_LF <- NULL
+fw$CO2E_SF<- NULL
+fw$CO2_BLDG <- NULL
+fw$CO2_ETOT <- NULL
+fw$CO2_MANF <- NULL
+fw$CO2_TRAN <- NULL
+fw$CO2_OTHX <- NULL
 
-tw_abs$CO2E_GF_KT...Value <- NULL
-tw_abs$CO2E_KD_GD...Value <- NULL
-tw_abs$CO2E_LF_KT...Value <- NULL
-tw_abs$CO2E_SF_KT...Value <- NULL
-tw_abs$CO2_BLDG_MT...Value <- NULL
-tw_abs$CO2_ETOT_MT...Value <- NULL
-tw_abs$ATM_CO2E_KT...Value <- NULL
-tw_abs$ATM_CO2E_PC...Value <- NULL
-tw_abs$CO2_MANF_MT...Value <- NULL
-tw_abs$CO2_OTHX_MT...Value <- NULL
-tw_abs$CO2_TRAN_MT...Value <- NULL
+tw$CO2E_GF_KT...Value <- NULL
+tw$CO2E_KD_GD...Value <- NULL
+tw$CO2E_LF_KT...Value <- NULL
+tw$CO2E_SF_KT...Value <- NULL
+tw$CO2_BLDG_MT...Value <- NULL
+tw$CO2_ETOT_MT...Value <- NULL
+tw$ATM_CO2E_KT...Value <- NULL
+tw$ATM_CO2E_PC...Value <- NULL
+tw$CO2_MANF_MT...Value <- NULL
+tw$CO2_OTHX_MT...Value <- NULL
+tw$CO2_TRAN_MT...Value <- NULL
 
-tw_rel$CO2E_EG <- NULL
-tw_rel$CO2E_GF <- NULL
-tw_rel$CO2E_LF <- NULL
-tw_rel$CO2E_SF<- NULL
-tw_rel$CO2_BLDG <- NULL
-tw_rel$CO2_ETOT <- NULL
-tw_rel$CO2_MANF <- NULL
-tw_rel$CO2_TRAN <- NULL
-tw_rel$CO2_OTHX <- NULL
+tw$CO2E_EG <- NULL
+tw$CO2E_GF <- NULL
+tw$CO2E_LF <- NULL
+tw$CO2E_SF<- NULL
+tw$CO2_BLDG <- NULL
+tw$CO2_ETOT <- NULL
+tw$CO2_MANF <- NULL
+tw$CO2_TRAN <- NULL
+tw$CO2_OTHX <- NULL
 
-fw_abs <- fw_abs[rowSums(is.na(fw_abs)) < ncol(fw_abs)/10,]
-tw_abs <- tw_abs[rowSums(is.na(tw_abs)) < ncol(tw_abs)/10,]
+# delete columns that contain duplicate information from fw_abs and tw_abs
+fw$AGR_TOTL_CN...Value <- NULL
+fw$AGR_TOTL_KD...Value <- NULL
+fw$AGR_TOTL_KN...Value <- NULL
+tw$AGR_TOTL_CN...Value <- NULL
+tw$AGR_TOTL_KD...Value <- NULL
+tw$AGR_TOTL_KN...Value <- NULL
 
-fw_rel <- fw_rel[rowSums(is.na(fw_rel)) < ncol(fw_rel)/10,]
-tw_rel <- tw_rel[rowSums(is.na(tw_rel)) < ncol(tw_rel)/10,]
+fw <- fw[rowSums(is.na(fw)) < ncol(fw)/10,]
+tw <- tw[rowSums(is.na(tw)) < ncol(tw)/10,]
+
+# reset the row names to the dates so that correlations can be calculated
+row.names(fw) <- fw$Row.names
+fw$Row.names <- NULL
+
+row.names(tw) <- tw$Row.names
+tw$Row.names <- NULL
+
+# try the correlation matrices
+cor_fw <- cor(fw)
+cor_tw <- cor(tw)
 
 # get lengths for training and holdout data sets
 # assign most recent 10 years (ie. last in the data) to the holdout
 
-fw_rel_training_length <- length(fw_rel$emission) - 10
-fw_abs_training_length <- length(fw_abs$emission) - 10
-tw_rel_training_length <- length(tw_rel$emission) - 10
-tw_abs_training_length <- length(tw_abs$emission) - 10
+fw_training_length <- length(fw$emission) - 10
+
+tw_training_length <- length(tw$emission) - 10
+
 
 # split data into training and holdout sets
 fw_rel_train <- fw_rel[1:fw_rel_training_length,]
@@ -103,20 +124,16 @@ tw_abs_train <- tw_abs[1:tw_abs_training_length,]
 tw_abs_holdout <- tail(tw_abs, n = 10)
 
 # Explorartory fits and variable selection
-attach(fw_abs)
-fw_abs_fit <- lm(formula = emission ~ ., data = fw_abs, na.action = na.omit) # all variable fit
-summary(fw_abs_fit)
-detach(fw_abs)
+attach(fw)
+fw_fit <- lm(formula = emission ~ ., data = fw, na.action = na.omit) # all variable fit
+summary(fw_fit)
+detach(fw)
 
 library(leaps)
-fw_abs_var_selection <- regsubsets(emission ~., data = fw_abs)
-summary(fw_abs_var_selection)
+fw_var_selection <- regsubsets(emission ~., data = fw)
+summary(fw_var_selection)
 tw_abs_var_selection <- regsubsets(emission ~., data = tw_abs)
 summary(tw_abs_var_selection)
-fw_rel_var_selection <- regsubsets(emission~.,data = fw_rel)
-summary(fw_rel_var_selection)
-tw_rel_var_selection <- regsubsets(emission ~., data = tw_rel)
-summary(tw_rel_var_selection)
 
 fw_abs_fit8 <- lm(emission ~ GHGO_KT_CE...Value + EN_URB_MCTY...Value + AGR_TRAC_NO...Value + AGR_TOTL_KN...Value + AGR_TOTL_CD...Value + AGR_TOTL_CN...Value + ADJ_DCO2_CD...Value + ADJ_DFOR_CD...Value, data = fw_abs)
 summary(fw_abs_fit8)
